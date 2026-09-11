e133ce54e4ad609b071f1d807a58b80d1ab78205
Marcelo Molina <juanmarcelomolina2@gmail.com>
2026-09-11 17:23:08 -0300

    Add login request validation

--- src/CorporateExpenses.Application/DTOs/Auth/LoginRequest.cs
+++ src/CorporateExpenses.Application/DTOs/Auth/LoginRequest.cs
@@ -1,5 +1,12 @@
-﻿namespace CorporateExpenses.Application.DTOs.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CorporateExpenses.Application.DTOs.Auth;
 
 public sealed record LoginRequest(
+    [Required]
+    [MinLength(3)]
     string Username,
+
+    [Required]
+    [MinLength(6)]
     string Password);
